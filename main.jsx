fd0f006ebbd89e363d66fbac44110f4b1519361d
midhun <iammidhunpj@gmail.com>
2016-03-15 15:21:18 +0530

    profile page view completed

--- main.jsx
+++ main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Profile, Skills, Education} from './modules/profile.jsx';
+import ProfileWrapper from './modules/profile.jsx';
 
 
-ReactDOM.render(<Profile />, document.getElementById('profile'))
-ReactDOM.render(<Skills />, document.getElementById('skills'))
-ReactDOM.render(<Education />, document.getElementById('edu'))
+ReactDOM.render(<ProfileWrapper />, document.getElementById('app'))
--- modules/profile.jsx
+++ modules/profile.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export class Profile extends React.Component {
+class Profile extends React.Component {
    render() {
     return(
-      <div className="profile_header">
+      <div className="profile_header card">
         <div className="pro_pic">
-          <img src="../assets/images/runner.png" width="100" height="100"/>
+          <img src="../assets/images/runner.png" width="150" height="150"/>
         </div>
         <div className="nameWrapper">
           <div className="name">
@@ -21,30 +21,45 @@ export class Profile extends React.Component {
   }
 };
 
-export class Skills extends React.Component {
-render() {
-return (
-  <div className="skill">
-    <div className="skillHead">Skills</div>
-    <div>
-      <p className="skills">skill1</p>
-      <p className="skills">skill2</p>
-    </div>
-  </div>
-);
-}
+class Skills extends React.Component {
+    render() {
+    return (
+      <div className="skill card">
+        <div className="skillHead">Skills</div>
+        <div>
+          <p className="skills">skill1</p>
+          <p className="skills">skill2</p>
+        </div>
+      </div>
+    );
+    }
 };
 
-export class Education extends React.Component {
-render() {
-return (
-  <div className="education">
-    <div className="eduHead">Education</div>
-    <div className="edu_list">
-      <div>Institution Name</div>
-      <div>Passout Year</div>
-    </div>
-  </div>
-);
-}
+class Education extends React.Component {
+    handlClick() {
+      debugger;
+    }
+    render() {
+    return (
+      <div className="education card">
+        <div className="eduHead" onClick={this.handlClick}>Education</div>
+        <div className="edu_list">
+          <div>Institution Name</div>
+          <div>Passout Year</div>
+        </div>
+      </div>
+    );
+    }
 };
+
+export default class ProfileWrapper extends React.Component {
+  render() {
+    return(
+      <div className= "container">
+        <Profile/>
+        <Skills/>
+        <Education/>
+      </div>
+    );
+  }
+}
