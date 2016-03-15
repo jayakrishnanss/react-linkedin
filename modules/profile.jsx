5b59b39cdcb0a757e8d0c272c49164a972ea6602
midhun <iammidhunpj@gmail.com>
2016-03-15 19:56:20 +0530

    add change profile pic feature style

--- modules/profile.jsx
+++ modules/profile.jsx
@@ -2,11 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class Profile extends React.Component {
-   render() {
+  constructor(props) {
+      super(props);
+      this.state = {
+          showChangeLink: false
+      };
+
+      this.mouseOver = this.mouseOver.bind(this);
+      this.mouseOut = this.mouseOut.bind(this);
+  }
+  mouseOver(){
+    this.setState({ showChangeLink: true })
+  }
+  mouseOut() {
+    this.setState({ showChangeLink: false })
+  }
+  render() {
+     let styleObj = {
+       "display": this.state.showChangeLink ? 'block' : 'none'
+     }
+
     return(
-      <div className="profile_header card">
+      <div className="profile_header card" onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
         <div className="pro_pic">
           <img src="../assets/images/runner.png" width="150" height="150"/>
+          <span className="changeProfile" style={styleObj}>Change your picture</span>
         </div>
         <div className="nameWrapper">
           <div className="name">
