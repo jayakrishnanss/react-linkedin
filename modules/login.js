56ec9f4436923ee872eeee5f3430243199da1436
shinyr <shinyr@qburst.com>
2016-03-15 10:23:51 +0530

    small fix

--- modules/login.js
+++ modules/login.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import DocumentTitle from 'react-document-title';
 
 export default class LoginPage extends React.Component {
+ 
   render() {
-    return (    
-        <div className="container">
-        <div className="row">
-        <div className="col-md-offset-5 col-md-3">
-            <div className="form-login">
-            <h4>Login Form</h4>
-            <input type="text" id="userName" className="form-control input-sm chat-input" placeholder="username" />
-            <br></br>
-            <input type="text" id="userPassword" className="form-control input-sm chat-input" placeholder="password" />
-            <br></br>
-            <div className="wrapper">
-            <span className="group-btn">     
-                <a href="#" className="btn btn-primary btn-md">login <i className="fa fa-sign-in"></i></a>
-            </span>
+    return (  
+        <form>  
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-offset-5 col-md-3">
+                        <div className="form-login">
+                            <h4>Login Form</h4>
+                            <input type="text" id="userName" className="form-control input-sm chat-input" placeholder="username" />
+                            <br></br>
+                            <input type="text" id="userPassword" className="form-control input-sm chat-input" placeholder="password" />
+                            <br></br>
+                            <div className="wrapper">
+                                <span className="group-btn">     
+                                    <button type="submit" className="btn btn-primary btn-md">login <i className="fa fa-sign-in"></i></button>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-    );
+        </form>
+    );    
   }
+
 }
