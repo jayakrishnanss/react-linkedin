cde15d5ef1989ab88bd5b682a353896e434b2250
Hari Krishnan <hari@qburst.com>
2016-03-16 15:59:08 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 8080
+
+app.use(express.static(path.join(__dirname, './')));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
+});
+
+app.listen(PORT, function() {
+  console.log("Server is up and running on port: " + PORT)
+});
