6b93cd541e14ba085b042882dbb27a6440553811
Jayakrishnan S <jayakrishnans@qburst.com>
2016-03-17 15:14:41 +0530

    header styling

--- js/modules/Header.jsx
+++ js/modules/Header.jsx
@@ -12,12 +12,26 @@ class Header extends React.Component {
       return (
         <div >
         	<div id="header">
-	            <a id="test_logo" href="/">
+	            <a id="linkedin_logo" href="/">
 	                <img src="/assets/images/Linkedin-icon.png" 
-	                alt="APIO logo" width="36" height="36" />
+	                alt="LinkedIn logo" width="36" height="36" />
 	            </a>
+	            <div id="noti_Container">
+				    <img width="36" height="36" src="/assets/images/contacts_icon.png" alt="message" />
+				    <div className="noti_bubble">3</div>
+				</div>
+	            <div id="noti_Container">
+				    <img width="36" height="36" src="/assets/images/message_icon.png" alt="message" />
+				    <div className="noti_bubble">2</div>
+				</div>	
             </div>
-            <div id="header-sub"></div>
+            <div id="header-sub">
+            	<ul>
+				  <li><a className="active" href="#">Home</a></li>
+				  <li><a href="#">Dashboard</a></li>
+				  <li><a href="#">Profile</a></li>
+				</ul>
+            </div>
         </div>
       );
    	}
