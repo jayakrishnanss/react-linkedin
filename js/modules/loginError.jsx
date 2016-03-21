2d76eee7900f8514f53bed7631dcca47c6e65038
shinyr <shinyr@qburst.com>
2016-03-21 14:02:46 +0530

    style change

--- js/modules/loginError.jsx
+++ js/modules/loginError.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 export default class LoginError extends React.Component {
 	render() {
 		return (
-			<div className="alert alert-danger">
-			 <a href="#" className="close" data-dismiss="alert" aria-label="close">&times;</a>
-			<strong>Error !</strong> Incorrect username or password.
+			<div className="row">
+				<div className="col-md-offset-4 col-md-4">
+					<div className="alert alert-danger">
+						<strong>Error !</strong> Incorrect username or password.
+					</div>
+				</div>
 			</div>
 			);
 	}
