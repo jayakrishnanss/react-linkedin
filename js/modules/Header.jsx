148209036b7fcfa590a8e33e897c6fc62823b79e
Abin Thaha <iamabinthaha@gmail.com>
2016-03-22 14:28:42 +0530

    Splitted components into different files

--- js/modules/Header.jsx
+++ js/modules/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import AppActions from '../actions/AppActions';
 import ContactStore from '../stores/contactStore';
-import Message from './message.jsx'
+import Message from './message.jsx';
+import ContactListEasyAccess from './HeaderContactsList.jsx';
 import HeaderActions from '../actions/HeaderMenuActions';
 import $ from "jquery";
 
@@ -18,14 +19,14 @@ class Header extends React.Component {
 		var newUsers = ContactStore.getNewUser();
 		newUsers = newUsers.reverse();
 		this.setState({contacts: newUsers});
-		// newUsers.forEach(function(user) {
-		// 	if (user.new_user == true) {
-		// 		newUserCount++;
-		// 		var obj = $.extend(true,{},user);
-		// 		obj.new_user = false;
-		//         AppActions.updateContact(obj);
-		// 	}
-		// });
+		newUsers.forEach(function(user) {
+			if (user.new_user == true) {
+				newUserCount++;
+				var obj = $.extend(true,{},user);
+				obj.new_user = false;
+		        AppActions.updateContact(obj);
+			}
+		});
 	}
 	componentDidMount() {
 	  	ContactStore.addChangeListener(this.onGetUser.bind(this));
@@ -79,52 +80,6 @@ class NotificationBubble extends React.Component {
 			<div className="noti_bubble">{this.props.count}</div>
 		)
 	}
-}
-
-class ContactListEasyAccess extends React.Component {
-	showNewlyAddedContacts() {
-		$('.contact_ul_wrapper').show();
-	}
-	hideNewlyAddedContacts() {
-		$('.contact_ul_wrapper').hide();
-	}
-	gotoContactPage() {
-		HeaderActions.clickHeaderMenu('MoreContacts');
-	}
-	render() {
-		if (this.props.users.length > 0) {
-			var userList = this.props.users.map(function(user, i) {
-	            return (
-	                <li key={user.id}>
-	                    <div className="new_user_details">
-							<img src="../../assets/images/user.jpg" />
-							<div className="user_name">{user.name}</div>
-	                    </div>
-	                </li>
-	            );
-	        }.bind(this));
-		}
-		else {
-			var userList =
-			<div className="spinner">
-				<img src="../../assets/images/rolling.gif"/>
-			</div>
-		}
-        return (
-            <div className="contact_ul_wrapper" onMouseEnter={this.showNewlyAddedContacts.bind(this)} onMouseLeave={this.hideNewlyAddedContacts.bind(this)}>
-				<div className="contact_header">
-					Contacts(0)
-					<span className="caret_msg">
-						<i className="fa fa-caret-right"></i>
-					</span>
-				</div>
-				<ul className="new_user_wrapper">
-					{userList}
-				</ul>
-				<div className="more_button" onClick={this.gotoContactPage}>More</div>
-            </div>
-        )
-    }
 }
 
 export default Header;
--- js/modules/contact.jsx
+++ js/modules/contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppActions from '../actions/AppActions';
 import ContactStore from '../stores/contactStore';
+import ContactForm from './AddContactForm.jsx';
 import $ from "jquery";
 
 var NoContacts = 0;
@@ -48,7 +49,6 @@ class ContactTable extends React.Component {
 	}
 	componentWillUnmount() {
         ContactStore.removeGetAllUserListenerr(this.onGetAllUser);
-        AppActions.getUsers('get_users');
 	}
     render() {
         return(
@@ -116,47 +116,6 @@ class AddContactButton extends React.Component {
         return(
             <div className="add-contact">
                 <button onClick={this.showPopup}>Add New</button>
-            </div>
-        )
-    }
-}
-
-class ContactForm extends React.Component{
-
-    hidePopup() {
-        $('.add_form_wrapper').hide();
-    }
-    addContact() {
-        var name = $('#name').val(),
-            email = $('#email').val();
-        var obj = {'id': NoContacts, 'name': name, 'email': email, 'new_user': true}
-        AppActions.addContactClick(obj);
-        $('.add_form_wrapper').hide();
-    }
-    updateUser() {
-        var name = $('#name').val(),
-            email = $('#email').val();
-        var obj = {'id': NoContacts, 'name': name, 'email': email, 'new_user': true}
-        AppActions.updateContact(obj);
-        $('.add_form_wrapper').hide();
-    }
-    render() {
-        return(
-            <div className="add_form_wrapper">
-                <form className="add_form" onSubmit={this.addContact}>
-                    <div className="content_wrapper">
-                        <label>Name : </label>
-                        <input type="text" id="name" placeholder="Name"/>
-                    </div>
-                    <div className="content_wrapper">
-                        <label>Email : </label>
-                        <input type="text" id="email" placeholder="Email"/>
-                    </div>
-                    <input className="button submit_button" type="submit" name="submit_button" value="Submit"/>
-                    <input className="button update_button" type="button" value="Update" onClick={this.updateUser}/>
-                    <input className="button cancel_button" type="button" name="cancel_button" onClick={this.hidePopup} value="Cancel"/>
-
-                </form>
             </div>
         )
     }
