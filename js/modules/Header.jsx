f7f1e31d6ba9e6653a421efdcebd508ac4183878
Abin Thaha <iamabinthaha@gmail.com>
2016-03-23 12:41:39 +0530

    Partial commit - Working on react router

--- js/modules/Header.jsx
+++ js/modules/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import AppActions from '../actions/AppActions';
-import ContactStore from '../stores/contactStore';
-import Message from './message.jsx';
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
 import ContactListEasyAccess from './HeaderContactsList.jsx';
+import Message from './message.jsx';
+import ProfileWrapper from './profileComponents/profile.jsx';
+
+import AppActions from '../actions/AppActions';
 import HeaderActions from '../actions/HeaderMenuActions';
+
+import ContactStore from '../stores/contactStore';
+
 import $ from "jquery";
 
 var newUserCount = 0;
@@ -12,19 +18,17 @@ class Header extends React.Component {
 	constructor(props, context) {
 	    super(props, context);
 		this.onGetUser = this.onGetUser.bind(this);
-		this.showNewlyAddedContacts = this.showNewlyAddedContacts.bind(this);
+		this.showNewlyAddedContacts.bind(this)
 		this.state = {contacts: []};
 	}
 	onGetUser() {
+		newUserCount = 0;
 		var newUsers = ContactStore.getNewUser();
 		newUsers = newUsers.reverse();
 		this.setState({contacts: newUsers});
 		newUsers.forEach(function(user) {
 			if (user.new_user == true) {
 				newUserCount++;
-				var obj = $.extend(true,{},user);
-				obj.new_user = false;
-		        AppActions.updateContact(obj);
 			}
 		});
 	}
@@ -52,7 +56,7 @@ class Header extends React.Component {
 	            <a id="linkedin_logo" href="/">
 	                <i className="fa fa-linkedin-square fa-2x"></i>
 	            </a>
-	            <div id="noti_Container" className="contact_icon" onMouseEnter={this.showNewlyAddedContacts.bind(this)}>
+	            <div id="noti_Container" className="contact_icon" onMouseEnter={this.showNewlyAddedContacts}>
 				    <i className="fa fa-user-plus fa-2x"></i>
 					<NotificationBubble count={newUserCount}/>
 					<ContactListEasyAccess users={this.state.contacts} />
@@ -64,9 +68,9 @@ class Header extends React.Component {
             </div>
             <div id="header-sub">
             	<ul>
-				  <li onClick={this.onClickMenu}><a className="active" href="#">Home</a></li>
-				  <li onClick={this.onClickMenu}><a href="#">Dashboard</a></li>
-				  <li onClick={this.onClickMenu}><a href="#">Profile</a></li>
+					<li onClick={this.onClickMenu}><a className="active" href="#">Home</a></li>
+				  	<li onClick={this.onClickMenu}><a href="#">Dashboard</a></li>
+				  	<li onClick={this.onClickMenu}><a href="#">Profile</a></li>
 				</ul>
             </div>
         </div>
